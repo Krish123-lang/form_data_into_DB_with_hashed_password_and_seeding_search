56b34f7c279b47c53d8ae435e7691a8a8b309f66
Krishna Mandal <56486342+Krish123-lang@users.noreply.github.com>
2023-08-02 12:24:15 +0545

    Add files via upload

---
+++ database/seeders/RegistrationSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+// Added manually
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class RegistrationSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('registrations')->insert([
+            'name' => Str::random(10),
+            'email' => Str::random(10) . '@gmail.com',
+            'password' => Hash::make('password'),
+        ]);
+    }
+}
